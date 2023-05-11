4ecefae6d3c46470913da74051743d9d74780aae
kirikirisu <b1801815@planet.kanazawa-it.ac.jp>
2023-05-11 17:51:02 +0900

    Answer practice 02

---
+++ practice/02/try/src/js/index.js
@@ -0,0 +1,15 @@
+(() => {
+  window.addEventListener("DOMContentLoaded", (event) => {
+    console.log("DOM fully loaded and parsed");
+    const button = document.getElementById("button");
+
+    const [dataType] = document.getElementsByClassName("text--type");
+    const [className] = document.getElementsByClassName("text--class");
+
+    button.onclick = () => {
+      console.log(button.dataset.type, button.className);
+      dataType.append(button.dataset.type);
+      className.append(button.className);
+    };
+  });
+})();

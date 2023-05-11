71ec0530b0f2d9c48d33fd62aa598725c47edbba
kirikirisu <b1801815@planet.kanazawa-it.ac.jp>
2023-05-11 18:39:15 +0900

    Answer practice04

---
+++ practice/04/try/src/js/index.js
@@ -0,0 +1,20 @@
+(() => {
+  const toggle = (buttonElement, targetToggle) => {
+    buttonElement.onclick = () => {
+      if (targetToggle.classList.value.split(" ").indexOf("is-active") === -1) {
+        targetToggle.classList.add("is-active");
+        return;
+      }
+      targetToggle.classList.remove("is-active");
+    };
+  };
+
+  window.addEventListener("DOMContentLoaded", (event) => {
+    const [aboveButton, bottomButton] =
+      document.getElementsByClassName("button");
+    const [aboveList, bottomList] = document.getElementsByClassName("list");
+
+    toggle(aboveButton, aboveList);
+    toggle(bottomButton, bottomList);
+  });
+})();

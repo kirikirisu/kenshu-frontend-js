1586a843d589731087b2bad3a1425ff0f9e5c219
kirikirisu <b1801815@planet.kanazawa-it.ac.jp>
2023-05-11 18:02:47 +0900

    chore

--- practice/02/try/src/js/index.js
+++ practice/02/try/src/js/index.js
@@ -6,7 +6,6 @@
     const [className] = document.getElementsByClassName("text--class");
 
     button.onclick = () => {
-      console.log(button.dataset.type, button.className);
       dataType.append(button.dataset.type);
       className.append(button.className);
     };

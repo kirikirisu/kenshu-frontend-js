ed51825951173b5c3845ae2769b0ef3d89fe6828
kirikirisu <b1801815@planet.kanazawa-it.ac.jp>
2023-05-12 14:55:31 +0900

    Use HTMLFormElement

--- practice/06/try/src/js/index.js
+++ practice/06/try/src/js/index.js
@@ -2,16 +2,12 @@ window.addEventListener("DOMContentLoaded", (event) => {
   const button = document.getElementById("button");
 
   button.addEventListener("click", () => {
-    const inputGenderList = document.getElementsByName("gender");
-    const inputName = document.forms.form.elements.name.value;
+    const form = document.getElementById("form");
+    const inputName = form.name.value;
 
     document.getElementById("text-wrapper").classList.add("is-active");
 
-    const checkedGender = document.querySelector(
-      'input[name="gender"]:checked'
-    ).value;
-
     document.getElementById("name-text").textContent = inputName;
-    document.getElementById("gender-text").textContent = checkedGender;
+    document.getElementById("gender-text").textContent = form.gender.value;
   });
 });

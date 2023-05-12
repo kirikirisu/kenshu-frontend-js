e2fde4514990501069a0f0a6ae578d2c031e6a29
kirikirisu <b1801815@planet.kanazawa-it.ac.jp>
2023-05-12 12:53:07 +0900

    Remove IIFE

--- practice/06/try/src/js/index.js
+++ practice/06/try/src/js/index.js
@@ -1,20 +1,18 @@
-(() => {
-  window.addEventListener("DOMContentLoaded", (event) => {
-    const button = document.getElementById("button");
+window.addEventListener("DOMContentLoaded", (event) => {
+  const button = document.getElementById("button");
 
-    button.onclick = () => {
-      const inputGenderList = document.getElementsByName("gender");
-      const inputName = document.forms.form.elements.name.value;
+  button.addEventListener("click", () => {
+    const inputGenderList = document.getElementsByName("gender");
+    const inputName = document.forms.form.elements.name.value;
 
-      document.getElementById("text-wrapper").classList.add("is-active");
+    document.getElementById("text-wrapper").classList.add("is-active");
 
-      let checkedGender = "";
-      inputGenderList.forEach((input) => {
-        if (input.checked) checkedGender = input.value;
-      });
+    let checkedGender = "";
+    inputGenderList.forEach((input) => {
+      if (input.checked) checkedGender = input.value;
+    });
 
-      document.getElementById("name-text").append(inputName);
-      document.getElementById("gender-text").append(checkedGender);
-    };
+    document.getElementById("name-text").textContent = inputName;
+    document.getElementById("gender-text").textContent = checkedGender;
   });
-})();
+});

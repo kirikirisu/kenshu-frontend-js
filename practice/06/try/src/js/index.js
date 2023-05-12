8de6dedfd39c7a48e9ce79f0e539da555fa44175
kirikirisu <b1801815@planet.kanazawa-it.ac.jp>
2023-05-12 10:23:26 +0900

    Answer 06

---
+++ practice/06/try/src/js/index.js
@@ -0,0 +1,20 @@
+(() => {
+  window.addEventListener("DOMContentLoaded", (event) => {
+    const button = document.getElementById("button");
+
+    button.onclick = () => {
+      const inputGenderList = document.getElementsByName("gender");
+      const inputName = document.forms.form.elements.name.value;
+
+      document.getElementById("text-wrapper").classList.add("is-active");
+
+      let checkedGender = "";
+      inputGenderList.forEach((input) => {
+        if (input.checked) checkedGender = input.value;
+      });
+
+      document.getElementById("name-text").append(inputName);
+      document.getElementById("gender-text").append(checkedGender);
+    };
+  });
+})();

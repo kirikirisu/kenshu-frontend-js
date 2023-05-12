21bb18fbf2166f7de3be7cec1467e53a6a2f04b0
kirikirisu <b1801815@planet.kanazawa-it.ac.jp>
2023-05-12 10:36:09 +0900

    Remove IIFE

--- practice/04/try/src/js/index.js
+++ practice/04/try/src/js/index.js
@@ -1,20 +1,17 @@
-(() => {
-  const toggle = (buttonElement, targetToggle) => {
-    buttonElement.onclick = () => {
-      if (targetToggle.classList.value.split(" ").indexOf("is-active") === -1) {
-        targetToggle.classList.add("is-active");
-        return;
-      }
-      targetToggle.classList.remove("is-active");
-    };
+const toggle = (buttonElement, targetToggle) => {
+  buttonElement.onclick = () => {
+    if (targetToggle.classList.value.split(" ").indexOf("is-active") === -1) {
+      targetToggle.classList.add("is-active");
+      return;
+    }
+    targetToggle.classList.remove("is-active");
   };
+};
 
-  window.addEventListener("DOMContentLoaded", (event) => {
-    const [aboveButton, bottomButton] =
-      document.getElementsByClassName("button");
-    const [aboveList, bottomList] = document.getElementsByClassName("list");
+window.addEventListener("DOMContentLoaded", (event) => {
+  const [aboveButton, bottomButton] = document.getElementsByClassName("button");
+  const [aboveList, bottomList] = document.getElementsByClassName("list");
 
-    toggle(aboveButton, aboveList);
-    toggle(bottomButton, bottomList);
-  });
-})();
+  toggle(aboveButton, aboveList);
+  toggle(bottomButton, bottomList);
+});

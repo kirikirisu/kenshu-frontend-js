3427096f3df579bb2e3cc060367dba06fb485b32
kirikirisu <b1801815@planet.kanazawa-it.ac.jp>
2023-05-15 17:32:36 +0900

    implement next

--- practice/10/try/src/js/index.js
+++ practice/10/try/src/js/index.js
@@ -3,18 +3,41 @@ window.addEventListener("DOMContentLoaded", (event) => {
   const [prev] = document.getElementsByClassName("prev");
   const [next] = document.getElementsByClassName("next");
 
-  const items = document.querySelectorAll(".item");
+  const slides = document.querySelectorAll(".item");
+
+  const elementFirstItem = document.querySelectorAll(".item")[0];
+  const copyElementFirstItem = elementFirstItem.cloneNode(true);
+  const setElementLastItem = document.querySelectorAll(".item")[slides.length];
+  list.insertBefore(copyElementFirstItem, setElementLastItem);
+
+  const elementLastItem = document.querySelectorAll(".item")[slides.length - 1];
+  const copyElementLastItem = elementLastItem.cloneNode(true);
+  const setElementFirstItem = document.querySelectorAll(".item")[0];
+  list.insertBefore(copyElementLastItem, setElementFirstItem);
 
   let curItem = 0;
-  let maxItem = items.length - 1;
+  let maxItem = slides.length - 1;
 
   next.addEventListener("click", function () {
-    if (curItem === maxItem) {
+    if (curItem === maxItem + 1) {
       curItem = 0;
-      list.style.left = `0px`;
+      list.style.transition = "left 0.3s";
+      list.style.left = `-400px`;
+      // list.style.transition = "";
     } else {
       curItem++;
-      list.style.left = `${-400 * curItem}px`;
+      list.style.transition = "left 0.3s";
+      list.style.left = `${-400 + -400 * curItem}px`;
+      // if (curItem === 3) {
+      //   list.style.transition = "left 0s";
+      // }
+    }
+  });
+  document.addEventListener("transitionend", () => {
+    if (curItem === 3) {
+      curItem = 0;
+      list.style.transition = "left 0s";
+      list.style.left = `-400px`;
     }
   });
 
@@ -22,9 +45,11 @@ window.addEventListener("DOMContentLoaded", (event) => {
     if (curItem === 0) {
       curItem = 2;
       list.style.left = `${-400 * maxItem}px`;
+      // list.style.transition = "";
     } else {
       curItem--;
       list.style.left = `${-400 * curItem}px`;
+      // list.style.transition = "all 0.3s";
     }
   });
 });
